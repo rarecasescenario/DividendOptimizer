0b48fe18f587d9041133a1d68690cd9def94e39e
rarecasescenario <rare.case.scenario@gmail.com>
2023-05-08 04:56:57 -0400

    got calculations for all on the watch list

--- Optimizer/src/main/java/com/stock/OptimizerApplicaiton.java
+++ Optimizer/src/main/java/com/stock/OptimizerApplicaiton.java
@@ -33,29 +33,42 @@ public class OptimizerApplicaiton {
 
 	// Calculate current assets
 	BigDecimal totalDividends = new BigDecimal(0);
-	System.out.println("Sumbol Price CurrentTotal QuoterlyDividends QuoterlyDivAmount CurrentYield UpperYield");
-	for (int i = 0; i < cpl.size(); i++) {
-	    final String symbol = cpl.get(i).getSymbol();
-	    System.out.println("symbol = " + symbol);
+	System.out.println(
+		"Sumbol Price CurrentTotal Shares QuoterlyDividends QuoterlyDivAmount CurrentYield UpperYield");
+
+	for (int i = 0; i < sl.size(); i++) {
+	    final String symbol = sl.get(i).getSymbol();
+	    // System.out.println("symbol = " + symbol);
 
 	    final Optional<SymbolCurrentState> scs = symbolCurrentState.stream()
 		    .filter(t -> t.getSymbol().equalsIgnoreCase(symbol)).findFirst();
-	    final Optional<WatchSymbol> ws = sl.stream().filter(t -> t.getSymbol().equalsIgnoreCase(symbol))
+
+	    final Optional<WatchSymbol> ws = sl.stream().filter(x -> x.getSymbol().equalsIgnoreCase(symbol))
 		    .findFirst();
 
-	    final BigDecimal totalAssets = BigDecimal.valueOf(cpl.get(i).getNumberOfShares())
-		    .multiply(scs.get().getPrice());
+	    BigDecimal totalAssets = new BigDecimal(0);
+	    BigDecimal symbolQuaterlyDividends = new BigDecimal(0);
+	    BigDecimal yield = new BigDecimal(0);
+	    int numberOfShares = 0;
 
-	    final BigDecimal symbolQuaterlyDividends = BigDecimal.valueOf(cpl.get(i).getNumberOfShares())
-		    .multiply(ws.get().getQuoterlyDividendAmount());
+	    final Optional<CurrentPosition> cplR = cpl.stream().filter(r -> r.getSymbol().equalsIgnoreCase(symbol))
+		    .findFirst();
 
-	    final BigDecimal yield = ws.get().quoterlyDividendAmount.multiply(BigDecimal.valueOf(400))
-		    .divide(scs.get().getPrice(), RoundingMode.HALF_EVEN);
+	    if (!cplR.isEmpty()) {
+		numberOfShares = cplR.get().getNumberOfShares();
 
-	    totalDividends = totalDividends.add(symbolQuaterlyDividends);
-	    System.out.println(symbol + " | " + scs.get().getPrice() + " | " + totalAssets + " | "
-		    + ws.get().getQuoterlyDividendAmount() + " | " + symbolQuaterlyDividends + " | " + yield + " | "
-		    + ws.get().getUpperYield());
+		totalAssets = BigDecimal.valueOf(numberOfShares).multiply(scs.get().getPrice());
+
+		symbolQuaterlyDividends = BigDecimal.valueOf(numberOfShares)
+			.multiply(ws.get().getQuoterlyDividendAmount());
+		totalDividends = totalDividends.add(symbolQuaterlyDividends);
+	    }
+	    yield = ws.get().quoterlyDividendAmount.multiply(BigDecimal.valueOf(400)).divide(scs.get().getPrice(),
+		    RoundingMode.HALF_EVEN);
+
+	    System.out.println(symbol + " | " + scs.get().getPrice() + " | " + totalAssets + " | " + numberOfShares
+		    + " | " + ws.get().getQuoterlyDividendAmount() + " | " + symbolQuaterlyDividends + " | " + yield
+		    + " | " + ws.get().getUpperYield() + "  > " + totalDividends);
 	}
 	System.out.println("TOTAL QUOTERLY DIVIDEND AMOUNT, $: " + totalDividends);
 
